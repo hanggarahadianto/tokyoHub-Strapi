35ca7aa6ca23551a28042bdfe337d93a3cbf7182
hangga rahadianto <hanggarahadianto@gmail.com>
2022-08-24 12:34:51 +0700

    second commit

---
+++ src/api/home-slide/controllers/home-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-slide controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-slide.home-slide');
---
+++ src/api/home-slide/routes/home-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-slide router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-slide.home-slide');
