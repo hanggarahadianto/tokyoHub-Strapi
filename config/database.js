aa6698bc9505c8e9aa09acaeb8b7167dc311e64a
hangga rahadianto <hanggarahadianto@gmail.com>
2022-08-24 16:29:13 +0700

    second commit

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'tokyoHub'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', '12345678'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "tokyoHub"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "12345678"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
